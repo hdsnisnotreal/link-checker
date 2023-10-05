adb3276526cc2f12b7bb9e924a311cd2c02f0d2c
wakeem <134740381+hdsnisnotreal@users.noreply.github.com>
2023-10-05 14:12:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
     const checkButton = document.getElementById("check-button");
     const linksInput = document.getElementById("links");
-    const resultsDiv = document.getElementById("results");
 
     checkButton.addEventListener("click", function () {
-        resultsDiv.textContent = ""; // Clear previous results
-
         const links = linksInput.value.trim().split("\n");
 
         links.forEach((link) => {
             const trimmedLink = link.trim();
-            const iframe = document.createElement("iframe");
+            const newTab = window.open(trimmedLink, "_blank");
 
-            // Set the iframe source to the link
-            iframe.src = trimmedLink;
-            iframe.style.display = "none"; // Hide the iframe
-
-            iframe.onload = () => {
-                // Check if the iframe loaded successfully
-                if (iframe.contentDocument && iframe.contentDocument.body) {
-                    const result = document.createElement("p");
-                    result.textContent = `${trimmedLink} - Not Blocked`;
-                    result.style.color = "green";
-                    resultsDiv.appendChild(result);
-                } else {
-                    const result = document.createElement("p");
-                    result.textContent = `${trimmedLink} - Blocked`;
-                    result.style.color = "red";
-                    resultsDiv.appendChild(result);
-                }
-                // Remove the iframe from the document
-                document.body.removeChild(iframe);
-            };
-
-            iframe.onerror = () => {
-                const result = document.createElement("p");
-                result.textContent = `${trimmedLink} - Blocked`;
-                result.style.color = "red";
-                resultsDiv.appendChild(result);
-                // Remove the iframe from the document
-                document.body.removeChild(iframe);
-            };
-
-            // Add the iframe to the document to trigger loading
-            document.body.appendChild(iframe);
+            if (!newTab || newTab.closed || typeof newTab.closed === "undefined") {
+                alert(trimmedLink + " - Blocked");
+            } else {
+                newTab.onload = function () {
+                    if (newTab.document.title === "Blocked Page" || newTab.document.body.innerHTML === "") {
+                        alert(trimmedLink + " - Blocked");
+                    } else {
+                        alert(trimmedLink + " - Not Blocked");
+                    }
+                    newTab.close();
+                };
+            }
         });
     });
 });
